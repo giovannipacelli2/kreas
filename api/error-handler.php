da6e03b435b9a62453488e1ad3f15ab48c1a0651
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-11-20 11:21:04 +0100

    Added time in error/exception

--- api/error-handler.php
+++ api/error-handler.php
@@ -7,7 +7,7 @@
     function errorHandler( $err_n, $err_str, $err_file, $err_line ){
 
         $date = new DateTime("now");
-        $date = $date->format( "Y-m-d" );
+        $date = $date->format( "Y-m-d H:i:s" );
         
         $message = "Date: $date" . PHP_EOL . "Error: [$err_n] $err_str - $err_file : $err_line" . PHP_EOL . PHP_EOL;
         error_log( $message, 3, "../error/error-log.txt" );
@@ -21,7 +21,7 @@
         $line = $e->getLine();
 
         $date = new DateTime("now");
-        $date = $date->format( "Y-m-d" );
+        $date = $date->format( "Y-m-d H:i:s" );
         
         $message = "Date: $date" . PHP_EOL . "Error: [$code] $msg - $file : line: $line" . PHP_EOL . PHP_EOL;
         error_log( $message, 3, "../error/exception-log.txt" );
