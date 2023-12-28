5681312ea227d987acd2d4c6db7c1266e23c73a2
giovannipacelli2 <giovannipacelli2@gmail.com>
2023-12-28 12:22:36 +0100

    run cs fixer

--- config.php
+++ config.php
@@ -3,9 +3,8 @@
 use Dotenv\Exception\InvalidPathException;
 use Dotenv\Exception\ValidationException;
 
-
 try {
-    $dotenv = Dotenv\Dotenv::createImmutable("./");
+    $dotenv = Dotenv\Dotenv::createImmutable('./');
 
     $dotenv->load();
 
@@ -13,28 +12,28 @@ try {
         'DB_NAME',
         'DB_USER',
         'DB_PASSWORD',
-        'DB_HOST'
+        'DB_HOST',
     ])->notEmpty();
 } catch (InvalidPathException $e) {
 
-    echo "<h2>Invalid credential</h2>";
+    echo '<h2>Invalid credential</h2>';
     http_response_code(401);
     exit();
 } catch (ValidationException $e) {
 
-    echo "<h2>Invalid credential</h2>";
+    echo '<h2>Invalid credential</h2>';
     http_response_code(401);
     exit();
 }
 
 return [
-    "database" => [
-        "name" => $_ENV["DB_NAME"],
-        "username" => $_ENV["DB_USER"],
-        "password" => $_ENV["DB_PASSWORD"],
-        "connection" => "mysql:host=" . $_ENV["DB_HOST"],
-        "options" => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]
-    ]
+    'database' => [
+        'name' => $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'connection' => 'mysql:host=' . $_ENV['DB_HOST'],
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ],
+    ],
 ];
--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 
-    require './vendor/autoload.php';
-    require './core/error-handler.php';
+require './vendor/autoload.php';
+require './core/error-handler.php';
 
-    use App\core\{ Request, Router };
+use App\core\Request;
+use App\core\Router;
 
-    $database = require './core/bootstrap.php';
+$database = require './core/bootstrap.php';
 
-    $method = Request::method();
-    $uri = Request::uri();
+$method = Request::method();
+$uri = Request::uri();
 
-    // Contains QUERY
-    //die(var_dump($_REQUEST));
+// Contains QUERY
+//die(var_dump($_REQUEST));
 
-    $router = Router::load( './app/routes.php' );
+$router = Router::load( './app/routes.php' );
 
-    $router->direct( $uri, $method );
-
-?>
+$router->direct( $uri, $method );
